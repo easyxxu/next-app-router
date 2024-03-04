bee75d6d34ccf447b401669bc83c4e7f87017d6d
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-03-04 22:59:00 +0900

    meals 리스트

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,11 +1,12 @@
 import MealItem from "./meal-item";
+import classes from "./meals-grid.module.css";
 
 export default function MealsGrid({ meals }) {
   return (
-    <ul>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
         <li key={meal.id}>
-          <MealItem {...meals} />
+          <MealItem {...meal} />
         </li>
       ))}
     </ul>
