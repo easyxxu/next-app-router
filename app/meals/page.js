66523ea45e6ccae8573097973ba10afb7e64f182
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-03-14 20:27:08 +0900

    Loading 컴포넌트 Suspense 적용

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,6 +2,7 @@ import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 import Link from "next/link";
 import { Suspense } from "react";
+import MealsLoadingPage from "./loading-out";
 import classes from "./page.module.css";
 
 async function Meals() {
@@ -25,9 +26,7 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <Suspense
-          fallback={<p className={classes.loading}>Fetching meals...</p>}
-        >
+        <Suspense fallback={<MealsLoadingPage />}>
           <Meals />
         </Suspense>
       </main>
