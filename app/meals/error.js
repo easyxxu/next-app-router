7c4c7931a5a60fefd71c221054fde5e118f7e151
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-03-14 20:45:41 +0900

    에러 처리

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function ErrorPage() {
+  return (
+    <main className="error">
+      <h1>Error</h1>
+      <p>Failed to fetch meal data. Please try again later</p>
+    </main>
+  );
+}
