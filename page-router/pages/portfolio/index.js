0f979a722eb755fd08d7e0b4cc33c7f768431b3a
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-04-12 17:02:59 +0900

    Link 설정

--- page-router/pages/portfolio/index.js
+++ page-router/pages/portfolio/index.js
@@ -1,7 +1,30 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Portfolio() {
+  const router = useRouter();
+
+  const projects = [
+    { id: 1, title: "Project 1" },
+    { id: 2, title: "Project 2" },
+  ];
   return (
     <div>
       <h1>Portfolio Page</h1>
+      <ul>
+        {projects.map((project) => (
+          <li>
+            <Link
+              href={{
+                pathname: "/portfolio/[projectId]",
+                query: { projectId: project.id },
+              }}
+            >
+              {project.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
