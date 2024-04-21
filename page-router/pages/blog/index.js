66b791c7194e9def8b84c290fcbecd8973833421
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-04-21 11:51:32 +0900

    feat: blog posts link

--- page-router/pages/blog/index.js
+++ page-router/pages/blog/index.js
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
+const BlogPosts = [
+  { title: "Post One", body: "This is post one" },
+  { title: "Post Two", body: "This is post two" },
+  { title: "Post Three", body: "This is post three" },
+];
+
 export default function Blog() {
   return (
     <div>
       <h1>Blog</h1>
+      <ul>
+        {BlogPosts.map((post, index) => (
+          <li key={index}>
+            <Link
+              href={{
+                pathname: "/blog/[slug]",
+                query: { slug: post.title },
+              }}
+            >
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
