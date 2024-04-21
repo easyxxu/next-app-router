6aef30c6e21c3fc6eeac1fcb1189153481a1da8f
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-04-21 11:50:56 +0900

    add home nav link

--- page-router/pages/index.js
+++ page-router/pages/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
