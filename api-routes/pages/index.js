ee6921d0b1ae7c8aaecd8023625b57da30f046ef
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-06-07 15:17:58 +0900

    feat: api route 활용

--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,7 +1,54 @@
+import { useState, useRef } from "react";
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+    console.log("reqBody: ", reqBody);
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
+  function loadFeedbackHandler() {
+    fetch("/api/feedback")
+      .then((res) => res.json())
+      .then((data) => setFeedbackItems(data.feedback));
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <label htmlFor="email">email :</label>
+        <input type="email" id="email" ref={emailInputRef} />
+        <br />
+        <label htmlFor="feedback">Feedback</label>
+        <textarea id="feedback" ref={feedbackInputRef} />
+        <button>Submit Feedback</button>
+      </form>
+      <button onClick={loadFeedbackHandler} type="button">
+        Load Feedback
+      </button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
